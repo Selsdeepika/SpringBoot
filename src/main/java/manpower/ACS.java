605b5ae2c74569a18afe1aa85e1f5d0756e3b884
Selva <selva.deepu24@gmail.com>
2019-08-29 05:27:11 -0700

    my first commit

---
+++ src/main/java/manpower/ACS.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package manpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lenovo
+ *
+ */
+@SpringBootApplication
+public class ACS {
+
+	/**
+	 * 
+	 */
+	public ACS() {
+		// TODO Auto-generated constructor stub
+		
+	System.out.println("deeps");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	System.out.println("deeps");
+		SpringApplication.run(ACS.class, args);
+	}
+
+}
